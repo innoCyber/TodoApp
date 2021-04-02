eed6053ceed7d6ccc353f920ca4716d3725e55d4
innocent <innocentileka@gmail.com>
2021-04-02 17:09:42 +0100

    *Add ListFragment menu items

--- app/src/main/java/com/altstoriesadminapp/todoapp/ui/fragments/list/ListFragment.kt
+++ app/src/main/java/com/altstoriesadminapp/todoapp/ui/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.altstoriesadminapp.todoapp.ui.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
 import com.altstoriesadminapp.todoapp.R
@@ -26,8 +24,14 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        setHasOptionsMenu(true)
+
         return view
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu,menu)
     }
 
 }
