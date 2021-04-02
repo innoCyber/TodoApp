de2226a1cafdc156723a9d3d9523e2f1da28ed42
innocent <innocentileka@gmail.com>
2021-04-02 17:20:50 +0100

    *Add UpdateFragment menu items

--- app/src/main/java/com/altstoriesadminapp/todoapp/ui/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/altstoriesadminapp/todoapp/ui/fragments/update/UpdateFragment.kt
@@ -1,20 +1,25 @@
 package com.altstoriesadminapp.todoapp.ui.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.altstoriesadminapp.todoapp.R
 
 class UpdateFragment : Fragment() {
-
+ 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu,menu)
     }
 
 }
