92a9b35dd090e4e3310cfdf98189bb0bb1a90084
innocent <innocentileka@gmail.com>
2021-04-03 20:32:47 +0100

    *Shared viewmodel

--- app/src/main/java/com/altstoriesadminapp/todoapp/ui/fragments/add/AddFragment.kt
+++ app/src/main/java/com/altstoriesadminapp/todoapp/ui/fragments/add/AddFragment.kt
@@ -3,6 +3,7 @@ package com.altstoriesadminapp.todoapp.ui.fragments.add
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.*
+import android.viewbinding.library.fragment.viewBinding
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -11,12 +12,14 @@ import com.altstoriesadminapp.todoapp.R
 import com.altstoriesadminapp.todoapp.data.entities.ToDoData
 import com.altstoriesadminapp.todoapp.data.model.Priority
 import com.altstoriesadminapp.todoapp.databinding.FragmentAddBinding
+import com.altstoriesadminapp.todoapp.viewmodel.SharedViewModel
 import com.altstoriesadminapp.todoapp.viewmodel.ToDoViewModel
 
 class AddFragment : Fragment(R.layout.fragment_add) {
 
-    val binding: FragmentAddBinding by viewModels()
-    val toDoViewModel: ToDoViewModel by viewModels()
+    private val binding: FragmentAddBinding by viewBinding()
+    private val toDoViewModel: ToDoViewModel by viewModels()
+    private val sharedViewModel: SharedViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -41,12 +44,12 @@ class AddFragment : Fragment(R.layout.fragment_add) {
         val priority = binding.priotitySpinner.selectedItem.toString()
         val description = binding.descriptionEdittext.text.toString()
 
-        val validation = verifyDataFromUser(title, description)
+        val validation = sharedViewModel.verifyDataFromUser(title, description)
         if (validation){
             val newData = ToDoData(
                 0,
                 title,
-                parsePriority(priority),
+                sharedViewModel.parsePriority(priority),
                 description
             )
 
@@ -55,21 +58,6 @@ class AddFragment : Fragment(R.layout.fragment_add) {
             findNavController().navigate(R.id.action_addFragment_to_listFragment)
         }else{
             Toast.makeText(requireContext(),"Fill all fields to continue ", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun verifyDataFromUser(title: String, description: String): Boolean{
-        return if(TextUtils.isEmpty(title) || TextUtils.isEmpty(description)){
-            false
-        }else !(title.isEmpty() || description.isEmpty())
-    }
-
-    private fun parsePriority(priority: String): Priority{
-        return when(priority){
-            "High Priority" -> Priority.HIGH
-            "Medium Priority" -> Priority.MEDIUM
-            "Low Priority" -> Priority.LOW
-            else -> Priority.LOW
         }
     }
 
