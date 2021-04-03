42291ec2787358b79fca2c2e301955fa71cda2f0
innocent <innocentileka@gmail.com>
2021-04-03 14:59:58 +0100

    *create database

---
+++ app/src/main/java/com/altstoriesadminapp/todoapp/ui/data/database/ToDoDatabase.kt
@@ -0,0 +1,36 @@
+package com.altstoriesadminapp.todoapp.ui.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.altstoriesadminapp.todoapp.ui.data.dao.ToDoDao
+import com.altstoriesadminapp.todoapp.ui.data.entities.ToDoData
+
+@Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase: RoomDatabase() {
+
+    abstract fun toDoDao(): ToDoDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: ToDoDatabase? = null
+
+        fun getDatabase(context: Context): ToDoDatabase{
+            val tempInstance = INSTANCE
+            if (tempInstance != null){
+                return tempInstance
+            }
+
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                ToDoDatabase::class.java,
+                "todo_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
